54a0edd9642ba0d7176dbf862ae2f73219741d8c
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2023-11-07 10:20:43 +0530

    initial commit for array examples

---
+++ com/arrays/Example1.java
@@ -0,0 +1,16 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+
+    }
+}
