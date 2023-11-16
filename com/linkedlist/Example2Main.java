85e50591d0affc922b32bef3503e91922af50d5d
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-11-16 09:27:44 +0530

    converting list to array method implemented

--- com/linkedlist/Example2Main.java
+++ com/linkedlist/Example2Main.java
@@ -1,5 +1,7 @@
 package com.linkedlist;
 
+import java.util.Arrays;
+
 public class Example2Main {
     public static void main(String[] args) {
         Example2 list = new Example2();
@@ -8,8 +10,12 @@ public class Example2Main {
         list.addLast(20);
         list.addLast(30);
         list.addLast(40);
+        int array[] = list.toArray();
+        System.out.println(Arrays.toString(array));
+
         list.removeFirst();
         list.removeLast();
+
         System.out.println(list.indexOf(40));
         System.out.println(list.contains(40));
         System.out.println(list.size());
