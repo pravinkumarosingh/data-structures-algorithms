93c17adb2f42188e9650bbc463b673b47dfd204e
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-11-18 12:48:25 +0530

    reversing string example added.

---
+++ com/stack/Example2.java
@@ -0,0 +1,11 @@
+package com.stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Example2 {
+    public static void main(String[] args) {
+        StringReverser str = new StringReverser();
+        System.out.println(str.reverse("abcd"));
+    }
+}
---
+++ com/stack/StringReverser.java
@@ -0,0 +1,23 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class StringReverser {
+    public String reverse(String input){
+        if(input == null){
+            throw new IllegalArgumentException();
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0;i<input.length();i++){
+            stack.push(input.charAt(i));
+        }
+
+        StringBuffer reversed = new StringBuffer();
+        for(int i=0;i<input.length();i++){
+            reversed.append(stack.pop());
+        }
+        return reversed.toString();
+    }
+}
