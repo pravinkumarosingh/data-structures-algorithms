413b3587ff2092bd30c02fae1c4df6d390f87a98
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-11-28 10:25:56 +0530

    Queue Reverser Class added.

---
+++ com/queue/Example3.java
@@ -0,0 +1,21 @@
+package com.queue;
+
+import com.queue.library.QueueReverser;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Example3 {
+    public static void main(String[] args) {
+        QueueReverser queueReverser = new QueueReverser();
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        System.out.println(queue);
+        System.out.println(queueReverser.reverse(queue,3));
+
+    }
+}
---
+++ com/queue/library/QueueReverser.java
@@ -0,0 +1,36 @@
+package com.queue.library;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueReverser {
+    private Queue<Integer> queue;
+
+    public QueueReverser(){
+        queue = new ArrayDeque<>();
+    }
+
+    public Queue<Integer> reverse(Queue<Integer> queue, int k){
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> tempQueue = new ArrayDeque<>();
+        while(k!=0){
+            stack.push(queue.remove());
+            --k;
+        }
+
+        while(!queue.isEmpty()){
+            tempQueue.add(queue.remove());
+        }
+
+        while(!stack.isEmpty()){
+            queue.add(stack.pop());
+        }
+
+        while (!tempQueue.isEmpty()){
+            queue.add(tempQueue.remove());
+        }
+
+        return queue;
+    }
+}
