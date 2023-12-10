a369e8aac46c8f5164322c1e0966b56105e802b3
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-12-10 10:55:30 +0530

    PriorityQueue class added and its implementation Example6 class

---
+++ com/queue/Example6.java
@@ -0,0 +1,23 @@
+package com.queue;
+
+import com.queue.library.PriorityQueue;
+
+public class Example6 {
+    public static void main(String[] args) {
+        PriorityQueue priorityQueue = new PriorityQueue();
+
+        priorityQueue.add(5);
+        priorityQueue.add(2);
+        priorityQueue.add(1);
+        priorityQueue.add(3);
+
+        System.out.println(priorityQueue.size());
+
+        System.out.println(priorityQueue.isEmpty());
+
+        while (!priorityQueue.isEmpty()){
+            System.out.println(priorityQueue.remove());
+        }
+
+    }
+}
---
+++ com/queue/library/PriorityQueue.java
@@ -0,0 +1,61 @@
+package com.queue.library;
+
+public class PriorityQueue {
+    private static int[] queue;
+
+    private static int count = 0;
+
+    private static int DEFAULT_CAPACITY = 5;
+
+    public PriorityQueue(){
+        queue = new int[DEFAULT_CAPACITY];
+    }
+
+    public PriorityQueue(int capacity){
+        queue = new int[capacity];
+    }
+
+    public void add(int element){
+        if(count == queue.length)
+            throw new IllegalStateException();
+
+        int i;
+        for(i=count-1;i>=0;i--){
+            if(queue[i]>element){
+                queue[i+1] = queue[i];
+            } else {
+                break;
+            }
+        }
+        queue[i+1] = element;
+        count++;
+    }
+
+    public int remove(){
+        if(count == 0) throw new IllegalStateException();
+
+        int[] temp = new int[queue.length - 1];
+
+        int first  = queue[0];
+
+        for (int i=0;i<temp.length;i++){
+            temp[i] = queue[i+1];
+        }
+        queue = temp;
+
+        --count;
+
+        return first;
+    }
+
+    public int size(){
+        return count;
+    }
+
+    public boolean isEmpty(){
+        if(queue.length==0){
+            return false;
+        }
+        return count == 0;
+    }
+}
