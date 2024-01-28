fd16dbddd8e3e6be4211c3990bd6cfb1a3b4a37b
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-01-28 17:26:31 +0530

    uncommented the code parts

--- com/tree/Example1.java
+++ com/tree/Example1.java
@@ -13,11 +13,11 @@ public class Example1 {
 
         System.out.println("Done inserting in Tree 1");
 
-//        Tree tree2 = new Tree();
-//
-//        tree2.insert(7);
-//        tree2.insert(4);
-//        tree2.insert(9);
+        Tree tree2 = new Tree();
+
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
 
         System.out.println("Done inserting in Tree 2");
 
@@ -34,7 +34,7 @@ public class Example1 {
 
         System.out.println("Height of the tree -> " + tree.height());
         System.out.println("Minimum value in a tree -> " + tree.min());
-        //System.out.println(tree.equals(tree2));
+        System.out.println(tree.equals(tree2));
 
         System.out.println("Is Binary Search Tree -> " + tree.isBinarySearchTree());
     }
